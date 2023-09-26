6a2e66375d41292a6cafc3d96ae631bc38bffdfc
Patrick Nguyen <patar.nguyen@gmail.com>
2023-09-26 10:59:56 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Patrick Nguyen on 2023-09-26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
